b624b4e3afa305eff425e254f25ee94529a40277
Ilya Bubnov <makebreathe1224@gmail.com>
2025-09-19 13:41:13 +0200

    added a font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
+
+const digitalNumbers = localFont({
+  variable: "--font-digital-numbers",
+  src: "../fonts/DigitalNumbers-Regular.ttf",
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +33,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <p className={digitalNumbers.className}>12.52 - testing</p>
         {children}
       </body>
     </html>
